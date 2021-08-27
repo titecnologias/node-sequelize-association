ff6b91a017fde3a9224420dd5eb0c98353cf3799
tilucass <ti.novastecnologias@gmail.com>
2021-08-27 01:19:12 -0300

    tudo fufando ainda

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -1,11 +1,11 @@
-const models = require("../models/start.models");
-const Tutorial = models.tutorials;
-const Comment = models.comments;
+const db = require("../models/start.models");
+const Tutorial = db.tutorials;
+const Comment = db.comments;
 
 //Create and Save new Tutorials
-
-exports.createTutorial = (tutorial) => {
-  return Tutorial.create({
+module.exports = {
+  async createTutorial(tutorial) {
+  await Tutorial.create({
     title: tutorial.title,
     description: tutorial.description,
   })
@@ -16,12 +16,12 @@ exports.createTutorial = (tutorial) => {
     .catch((err) => {
       console.log(">> Error while creating tutorial: ", err);
     });
-};
+},
 
 //Create and Save new Comments
 
-exports.createComment = (tutorialId, comment) => {
-  return Comment.create({
+async createComment(tutorialId, comment) {
+  await Comment.create({
     name: comment.name,
     text: comment.text,
     tutorialId: tutorialId,
@@ -33,38 +33,39 @@ exports.createComment = (tutorialId, comment) => {
     .catch((err) => {
       console.log(">> Error while creating comment: ", err);
     });
-};
+},
 
 //Get the comments for a given tutorial
 
-exports.findTutorialById = (tutorialId) => {
-  return Tutorial.findByPk(tutorialId, { include: ["comments"] })
+async findTutorialById(tutorialId) {
+  await Tutorial.findByPk(tutorialId, { include: ["comments"] })
     .then((tutorial) => {
       return tutorial;
     })
     .catch((err) => {
       console.log(">> Error while finding tutorial: ", err);
     });
-};
+},
 
 //Get the comments for a given comment id
 
-exports.findCommentById = (id) => {
-  return Comment.findByPk(id, { include: ["tutorial"] })
+async findCommentById(id) {
+  await Comment.findByPk(id, { include: ["tutorial"] })
     .then((comment) => {
       return comment;
     })
     .catch((err) => {
       console.log(">> Error while finding comment: ", err);
     });
-};
+},
 
 //Get all Tutorials include comments
 
-exports.findAll = () => {
-  return Tutorial.findAll({
+async findAll() {
+  await Tutorial.findAll({
     include: ["comments"],
   }).then((tutorials) => {
     return tutorials;
   });
+},
 };
